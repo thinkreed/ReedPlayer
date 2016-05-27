9c797719da94ffd966b3ee0612511ebb17da92e8
thinkreed <thinkreed2016@gmail.com>
2016-05-27 18:02:16 +0800

    add immersive mode for player activity

--- app/src/main/java/com/reed/reedplayer/activity/PlayActivity.java
+++ app/src/main/java/com/reed/reedplayer/activity/PlayActivity.java
@@ -1,6 +1,7 @@
 package com.reed.reedplayer.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.reed.reedplayer.fragment.BaseFragment;
 import com.reed.reedplayer.fragment.PlayerFragment;
@@ -13,5 +14,19 @@ public class PlayActivity extends BaseActivity {
   protected BaseFragment getFragment() {
     Bundle bundle = getIntent().getExtras();
     return PlayerFragment.getInstance(bundle);
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      getWindow().getDecorView().setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
   }
 }
